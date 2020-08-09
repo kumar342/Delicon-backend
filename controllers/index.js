bf6fdf1c9f03e80b2d143c344b131477cb292bbe
Hemanth Kumar <56812697+kumar342@users.noreply.github.com>
2020-08-09 22:05:41 +0530

    crud added

--- controllers/index.js
+++ controllers/index.js
@@ -11,7 +11,6 @@ const totalReservations = (req, res) => {
 };
 
 const addReservation = (req, res) => {
-  console.log("value", req.body);
   ReservationRoom.create({
     name: req.body.name,
     email: req.body.email,
@@ -28,7 +27,52 @@ const addReservation = (req, res) => {
     });
 };
 
+const totalReservationsById = (req, res) => {
+  const reservationId = ReservationRoom.findById(req.params.Id)
+    .then((reservationId) => {
+      res.json(reservationId);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+};
+
+const reservationDeleteById = (req, res) => {
+  const deleteReservation = ReservationRoom.remove({ _id: req.params.Id })
+    .then((deleteReservation) => {
+      res.json(deleteReservation);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+};
+
+const reservationUpdateById = (req, res) => {
+  const updateReservation = ReservationRoom.updateOne(
+    { _id: req.params.Id },
+    {
+      $set: {
+        name: req.body.name,
+        email: req.body.email,
+        mobile_number: req.body.mobile_number,
+        hotel_name: req.body.hotel_name,
+        noOfRoomsBooked: req.body.noOfRoomsBooked,
+        noOfRoomsAvailable: req.body.noOfRoomsAvailable,
+      },
+    }
+  )
+    .then((updateReservation) => {
+      res.json(updateReservation);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+};
+
 module.exports = {
   totalReservations,
   addReservation,
+  totalReservationsById,
+  reservationDeleteById,
+  reservationUpdateById,
 };
--- routes.js
+++ routes.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const routes = express.Router();
-const { totalReservations, addReservation } = require("./controllers/index");
+const {
+  totalReservations,
+  addReservation,
+  totalReservationsById,
+  reservationDeleteById,
+  reservationUpdateById,
+} = require("./controllers/index");
 
-routes.get("/", totalReservations);
+routes.get("/totalReservations", totalReservations);
+routes.get("/totalReservations/:Id", totalReservationsById);
 routes.post("/add", addReservation);
+routes.delete("/totalReservations/:Id", reservationDeleteById);
+routes.put("/totalReservations/:Id", reservationUpdateById);
 
 module.exports = routes;
